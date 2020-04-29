6785c801f92cac31a352c983c4c8b20ec6ea3c3e
RainbowVom <webster123web@gmail.com>
2020-04-29 15:04:43 -0400

    bug fix for ghost precincts relating to errors

--- src/main/java/server/ErrorRepo.java
+++ src/main/java/server/ErrorRepo.java
@@ -17,7 +17,7 @@ import org.springframework.data.repository.query.Param;
 public interface ErrorRepo extends JpaRepository<Error, Integer> {
     //A Native Query means it is a SQL statement, normally by default it is JPQL Queries which can only perform CRUD queries.
     //The : binds the variable stateFP to the param stateFP to dynamically load errors from specific states
-    @Query(value="SELECT * FROM error e join precinct p WHERE p.OGR_FID=e.precinct_id AND p.statefp= stateFP",nativeQuery=true)
+    @Query(value="SELECT * FROM error e join precinct p WHERE p.OGR_FID=e.precinct_id AND p.statefp= :stateFP",nativeQuery=true)
     List<Error> findByStatefp(@Param("stateFP") String stateFP);
     Error findByPrecinctID(Integer precinctID);
 }
