520cdd2f982bb63476d3d7fc0fdd9d30b084f0fc
RainbowVom <webster123web@gmail.com>
2020-04-28 20:35:03 -0400

    Revert "small bug fix when preparing error to send & setting precinct id from error controller"
    This reverts commit fbe6ff65360ea654e27d0abb2d62c880ce4fa9cf.

--- src/main/java/server/ErrorController.java
+++ src/main/java/server/ErrorController.java
@@ -42,7 +42,6 @@ public class ErrorController {
         try {
             Error existError = service.findByPrecinctID(Integer.parseInt(id));
             error.setErrorID(existError.getErrorID());
-            error.setPrecinctID(existError.getPrecinctID());
             error.setCommentTime(new Timestamp(System.currentTimeMillis()));
             service.save(error);
         } catch (NoSuchElementException e) {
