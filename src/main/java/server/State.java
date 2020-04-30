00200ca413bb046fafc0f1655bcb3b5d5ed3d558
RainbowVom <webster123web@gmail.com>
2020-04-30 15:15:25 -0400

    implemented state get

---
+++ src/main/java/server/State.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author webst
+ */
+@Entity
+@Table(name="STATE")
+public class State {
+    private Integer ogrFID;
+    private String statefp;
+    private String state_geojson;
+    private String name;
+    
+    public State(){
+        super();
+    }
+    public State(Integer ogrFID, String statefp, String state_geojson, String name){
+        this.ogrFID=ogrFID;
+        this.statefp=statefp;
+        this.state_geojson=state_geojson;
+        this.name=name;
+    }
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="OGR_FID")
+    public Integer getOgrFID() {
+        return ogrFID;
+    }
+
+    @Column(name="statefp")
+    public String getStatefp() {
+        return statefp;
+    }
+
+    @Column(name="state_geojson")
+    public String getState_geojson() {
+        return state_geojson;
+    }
+
+    @Column(name="name")
+    public String getName() {
+        return name;
+    }
+    
+    public void setOgrFID(Integer ogrFID) {
+        this.ogrFID = ogrFID;
+    }
+
+    public void setStatefp(String statefp) {
+        this.statefp = statefp;
+    }
+
+    public void setState_geojson(String state_geojson) {
+        this.state_geojson = state_geojson;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/server/StateController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author webst
+ */
+@RestController
+public class StateController {
+    @Autowired
+    private StateRepo service;
+    
+    @GetMapping("/states")
+    public List<State> list() {
+        return service.findAll();
+    }
+}
